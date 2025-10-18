142920fa91d1492cbbc4731884f7894add0ed54f
Eduardo Orihuela <e.orihuelaverdugo@metro-markets.de>
2025-10-18 13:58:14 +0200

    main: Update Layout component

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,22 +1,23 @@
 import type { PropsWithChildren } from 'react';
+import { Link } from 'react-router-dom';
 import { SVGIcon } from '../SVGIcon/SVGIcon';
+import './Layout.scss';
 
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className="layout">
-      <header className="layout-header">
-        <div className="header-content">
-          <nav className="navigation">
-            <a href="/" className="nav-link">
-              <SVGIcon src="/src/assets/mbst.svg" width="96px" height="32px" />
-            </a>
-            <a href="/cart" className="nav-link">
-              <SVGIcon src="/src/assets/bag.svg" width="18px" height="22px" />
-            </a>
-          </nav>
-        </div>
+      <header className="layout__header">
+        <nav className="layout__nav">
+          <Link to="/" className="layout__nav-link">
+            <SVGIcon src="/src/assets/mbst.svg" width="96px" height="32px" />
+          </Link>
+          <Link to="/cart" className="layout__nav-cart">
+            <SVGIcon src="/src/assets/bag.svg" width="18px" height="22px" />
+            <span className="layout__nav-cart-items">0</span>
+          </Link>
+        </nav>
       </header>
-      <main className="main-content">{children}</main>
+      <main className="layout__main">{children}</main>
     </div>
   );
 };
